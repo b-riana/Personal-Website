c6106087582bc852f856fd9ecde8806ecb1c35f1
b-riana <brirosado05@gmail.com>
2025-08-17 20:51:12 -0400

    commit

--- main.js
+++ main.js
@@ -34,3 +34,67 @@ document.addEventListener('DOMContentLoaded', () =>{
 
 
 });
+
+// Project filtering
+document.addEventListener('DOMContentLoaded', () =>{
+    const allType = document.querySelector('.type.all');
+    const pythonType = document.querySelector('.type.python');
+    const engType = document.querySelector('.type.engineering');
+    const javaType = document.querySelector('.type.java');
+
+    const pythonCard = document.querySelector('.card.python');
+    const engCard = document.querySelector('.card.engineering');
+    const javaCard = document.querySelector('.card.java');
+
+    
+    const typeContainer = document.querySelector('.types');
+
+    typeContainer.addEventListener('click', (e) => {
+        const typePick = e.target.closest('.type');
+
+        if (!typePick) return; // does nothing if header is not the thing clicked
+
+        // Toggle visibility of body
+        typePick.classList.toggle('active');
+
+        // Unselects other types
+        const otherTypes = typeContainer.querySelectorAll('.type');
+        otherTypes.forEach((otherType) => {
+            if(otherType != typePick){
+                otherType.classList.remove('active');
+            }
+        });
+
+        const allCards = document.querySelectorAll('.card');
+        allCards.forEach((thisCard) => {
+            if (typePick.classList.contains('python')){
+                if (thisCard.classList.contains('python')){
+                    thisCard.classList.add('active');
+                } else {    
+                    thisCard.classList.remove('active');
+                }
+            } else if (typePick.classList.contains('all')){
+                if (thisCard.classList.contains('all')){
+                    thisCard.classList.add('active');
+                } else {    
+                    thisCard.classList.remove('active');
+                }
+            } else if (typePick.classList.contains('engineering')){
+                if (thisCard.classList.contains('engineering')){
+                    thisCard.classList.add('active');
+                } else {    
+                    thisCard.classList.remove('active');
+                }
+            } else {
+                if (thisCard.classList.contains('java')){
+                    thisCard.classList.add('active');
+                } else {    
+                    thisCard.classList.remove('active');
+                }
+            }
+
+        });
+        
+    })
+
+});
